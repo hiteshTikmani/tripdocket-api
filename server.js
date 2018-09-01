dbb0395772ffc89f066f67fe4f7f40ca61db8f7d
Hitesh Tikmani <1994hiteshtikmani@gmail.com>
2018-09-01 18:28:08 +0530

    multiple markers and css/style

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+// var cors = require(cors);
+const { Schema } = mongoose;
 
 app.use(bodyParser.json());
+// app.use(cors());
 
 var router = express.Router();
 
@@ -13,5 +16,32 @@ router.get('/',(req,res) => {
     res.send({"hitesh":"tikmani"});
 });
 
-var PORT = 8080;
+// router.get('/db', (req, res) => {
+//     res.send(connect());
+// })
+
+
+const MONGO_URI = 'mongodb://hiteshtikmani:hsetih123@ds129541.mlab.com:29541/travel-test-db'; 
+
+mongoose.connect(MONGO_URI,(err,db)=>{
+    if(err){console.log('ERROR AA GYA BHAI')};
+    db.collection('places-test').findOne({},(err,result)=>{
+        if(err){console.log('Coolection mein ERROR AA GYA BHAI')}
+        router.get('/db',(req,res) => {
+            res.send(result);
+        });
+    })
+});
+
+// let connect = async function getRoute() {
+//     try {
+//         let db = await mongoose.connect(MONGO_URI);
+//         console.log(db);
+//         // let res = await db.collection('places-test').findOne({});
+//     } catch (error) {
+//         console.log(error);
+//     }   
+// }
+
+const PORT = 8000;
 app.listen(PORT);
