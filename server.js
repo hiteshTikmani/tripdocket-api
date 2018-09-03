3bfb0b276be7a55426181856beece51cb17fc904
Hitesh Tikmani <1994hiteshtikmani@gmail.com>
2018-09-04 03:04:39 +0530

    fix

--- server.js
+++ server.js
@@ -27,12 +27,12 @@ mongoose.connect(keys.mongoURI,(err,db)=>{
 });
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/buid'));
+    app.use(express.static('client/build'));
 
-    // const path = require('path');
-    // app.get('*',(req,res)=>{
-    //     res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    // });
+    const path = require('path');
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    });
 }
 
 const PORT = process.env.PORT || 8000;
